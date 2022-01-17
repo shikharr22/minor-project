4ab6d57d1c460f6fd8bdcfc3640b6925a5b05382
shikharr22 <shikharraizaday@gmail.com>
2022-01-18 03:32:17 +0530

    Added humid temp and moisture arrays

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,23 +1,49 @@
 import { React, useEffect, useState } from "react";
 import "./App.css";
 import { getDatabase, ref, child, get } from "firebase/database";
-import database from './Firebase.js';
+import database from "./Firebase.js";
 const HomePage = () => {
-
-    
-   useEffect(()=>{
+  const [data, setData] = useState([]);
+  const fetchData = async () => {
     const dbRef = ref(database);
-    get(child(dbRef, `Data`)).then((snapshot) => {
-      if (snapshot.exists()) {
-        console.log(snapshot.val());
-      } else {
-        console.log("No data available");
-      }
-    }).catch((error) => {
-      console.error(error);
-    });
-   },[])
-    
+    get(child(dbRef, `Data`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          let array = [];
+          snapshot.forEach((element) => {
+            array.push(element.val());
+          });
+          setData({ array: array });
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  let humid = [],
+    temp = [],
+    moisture = [];
+
+  console.log(data.array);
+  if (data.array != undefined) {
+    for (let i = 0; i < 6; i++) {
+      humid.push(data.array[i].Humid);
+      temp.push(data.array[i].Temp);
+      moisture.push(data.array[i].Moisture);
+    }
+
+    console.log(`Humidity:${humid}`);
+
+    console.log(`Temperature:${temp}`);
+
+    console.log(`Moisture: ${moisture}`);
+  }
 
   return (
     <>
